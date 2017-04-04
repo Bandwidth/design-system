7a851a5f903d5c638e8933ccadbdb2e6d4c3f199
Dan Tolbert <dtolbert@bandwidth.com>
2017-04-04 16:06:49 -0400

    remove option to fail out for rm-rf

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf out/**/*
 ls -la out/
 
 # Run our compile script
