f5fd41a688bbcf7f4e3c032d440fdd66b5d5d931
Travis CI <dtolbert@bandwidth.com>
2017-05-09 00:24:40 +0000

    Deploy to GitHub Pages: 815e0797012c0afdaaad3a8f56fa3dff8af92c29

---
+++ source/js/pagination.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+    // Finding total number of pages
+    var pages = $('.pagination__item--page').length;
+    var pagesLast = pages - 5;
+
+    // Clicking on a page number
+    $('.pagination__item--page').click(function(){
+        $('.pagination__item--page').removeClass('active');
+        $('.pagination__item--page').hide();
+        $(this).addClass('active');
+        $('.pagination__item--prev').css('visibility','visible');
+        $('.pagination__item--next').css('visibility','visible');
+        if ($(this).val() == 1){
+          $('.pagination__item--page').slice(0,10).show();
+          $('.pagination__item--prev').css('visibility','hidden');
+        } else if ($(this).val() <= 5 && $(this).val() > 1){
+          $('.pagination__item--page').slice(0,10).show();
+        } else if ($(this).val() < pages && $(this).val() >= pagesLast) {
+          $(this).show();
+          $('.pagination__item--page').slice(-10).show();
+        } else if ($(this).val() == pages){
+          $('.pagination__item--next').css('visibility','hidden');
+          $('.pagination__item--page').slice(-10).show();
+        } else {
+          $(this).show();
+          $(this).nextAll().slice(0,4).show();
+          $(this).prevAll().slice(0,5).show();
+        }
+    });
+
+    // Clicking on the previous button
+    $('.pagination__item--prev').click(function(){
+        var currentPage = $('.pagination__item--page.active');
+        var currentPageVal = $('.pagination__item--page.active').val();
+        $('.active').prev().addClass('active');
+        currentPage.removeClass('active');
+        $('.pagination__item--next').css('visibility','visible');
+        if (currentPageVal === 2){
+          $('.pagination__item--prev').css('visibility','hidden');
+        } else if (currentPageVal <= 6){
+          $('.pagination__item--page').hide();
+          $('.pagination__item--page').slice(0,10).show();
+        } else if (currentPageVal > 6 && currentPageVal <= pagesLast){
+          $('.pagination__item--page').hide();
+          $('.active').show();
+          $('.active').nextAll().slice(0,4).show();
+          $('.active').prevAll().slice(0,5).show();
+        } else if (currentPageVal < pagesLast){
+          $('.pagination__item--page').slice(-10).show();
+        }
+    });
+
+    // Clicking on the next button
+    $('.pagination__item--next').click(function(){
+        var currentPage = $('.pagination__item--page.active');
+        var currentPageVal = $('.pagination__item--page.active').val();
+        $('.active').next().addClass('active');
+        currentPage.removeClass('active');
+        $('.pagination__item--prev').css('visibility','visible');
+        if (currentPageVal < 5){
+          $('.pagination__item--page').hide();
+          $('.pagination__item--page').slice(0,10).show();
+        } else if (currentPageVal > 5 && currentPageVal < pagesLast){
+          $('.pagination__item--page').hide();
+          $('.active').show();
+          $('.active').nextAll().slice(0,4).show();
+          $('.active').prevAll().slice(0,5).show();
+        } else if (currentPageVal >= pagesLast && currentPageVal < (pages - 1)){
+          $('.pagination__item--page').hide();
+          $('.pagination__item--page').slice(-10).show();
+        } else if (currentPageVal === (pages - 1)){
+          $('.pagination__item--next').css('visibility','hidden');
+        }
+    });
+});
